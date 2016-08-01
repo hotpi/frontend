003587f24cefd6a8f694b65b8413c2e9f9efabdf
juan <juanecabellob@gmail.com>
2016-08-01 18:49:31 +0200

    Abstraction of NoteLineOptions done. Missing: - Views for the different types of notes.

--- NoteLine.js
+++ NoteLine.js
@@ -1,21 +1,14 @@
 import React from 'react';
 
+import _ from 'lodash';
+
 import TextField from 'material-ui/TextField';
 import IconButton from 'material-ui/IconButton';
-import IconMenu from 'material-ui/IconMenu';
-import MenuItem from 'material-ui/MenuItem';
-import Avatar from 'material-ui/Avatar';
-import Chip from 'material-ui/Chip';
 import Badge from 'material-ui/Badge';
 
 import ContentAdd from 'material-ui/svg-icons/content/add';
-import ActionTouchApp from 'material-ui/svg-icons/action/touch-app';
 import NavigationCancel from 'material-ui/svg-icons/navigation/cancel';
-import ToggleStar from 'material-ui/svg-icons/toggle/star';
-import EditorHighlight from 'material-ui/svg-icons/editor/highlight';
-import ContentMoveToInbox from 'material-ui/svg-icons/content/move-to-inbox';
 import NavigationMoreVert from 'material-ui/svg-icons/navigation/more-vert';
-import ImageLens from 'material-ui/svg-icons/image/lens';
 
 import {
   amber700,
@@ -30,8 +23,10 @@ import {
   grey400
 } from 'material-ui/styles/colors'
 
-const importantColors = [grey400, amber700, amber400, amber100];
-const highlightColors = [grey400, yellowA100, blueA100, greenA100, orangeA100, cyanA100];
+import NoteLineOptions from './NoteLineOptions'
+
+export const importantColors = ["transparent", amber700, amber400, amber100];
+export const highlightColors = ["transparent", yellowA100, blueA100, greenA100, orangeA100, cyanA100];
 
 const lineOutHover = {
   notLast: {
@@ -43,7 +38,7 @@ const lineOutHover = {
   }
 }
 
-const iconStyles = {
+export const iconStyles = {
   icon: {
     width: '18px',
     height: '18px',
@@ -61,7 +56,6 @@ const iconStyles = {
   }
 };
 
-//TODO: Object assign?
 const inlineIconStyle = Object.assign({}, iconStyles, {
   iconArea: {
     padding: 0,
@@ -83,7 +77,7 @@ export default class NoteLine extends React.Component {
       options: {
         important: {
           set: false,
-          color: amber400,
+          color: "transparent",
           value: 0
         },
         reminder: {
@@ -105,6 +99,22 @@ export default class NoteLine extends React.Component {
     }
   }
 
+  isHighlighted() {
+    return this.state.options.highlight.set;
+  }
+
+  isImportant() {
+    return this.state.options.important.set;
+  }
+
+  updateOptions(optionsToUpdate) {
+    let options = {important: this.state.options.important, highlight: this.state.options.highlight, reminder: this.state.options.reminder}
+    let optionsUntouched = _.omit(options, _.keys(optionsToUpdate));
+    let newOptions = _.assign(optionsUntouched, optionsToUpdate);
+
+    this.setState({options: newOptions})
+  }
+
   handleKeyDown(e) {
     if (e.keyCode === 13) {
       e.preventDefault();  
@@ -119,134 +129,6 @@ export default class NoteLine extends React.Component {
         return this.props.deleteLine();
       }
     } 
-  }
-
-  showCancelButton() {
-    if (!this.state.last) {
-      return (
-        <IconButton
-          tooltip="Delete line"
-          className="line-buttons"
-          style={inlineIconStyle.iconArea} 
-          iconStyle={inlineIconStyle.icon} 
-          onClick={this.handleClickDelete.bind(this)} >
-          <NavigationCancel />
-        </IconButton>
-      );
-    }
-  }
-
-  showAddIcon() {
-    if (this.state.last) {
-      return (
-        <ContentAdd style={{left: 5, height: 18, width: 18, color: 'grey', paddingTop: 13, paddingRight: 2, marginRight: 5, marginLeft: 5}}/>
-      );
-    }
-  }
-
-  isHighlighted() {
-    return this.state.options.highlight.set;
-  }
-
-  isImportant() {
-    return this.state.options.important.set;
-  }
-
-  willBeReminded() {
-    return false;//this.state.options.reminder.set;
-  }
-
-  showChips() {
-    if (this.willBeReminded()) {
-      return (
-        <Chip 
-          style={{height: 20, marginTop: 7, backgroundColor: this.state.options.important.color}}
-          labelStyle={{padding: 4, top: 14, margin: 4, marginTop: 0, paddingTop: 0, lineHeight: '1.6em', height: 20}}>
-      </Chip>)
-    }
-/*
-    if (this.willBeReminded()) {
-      chips = chip || <Chip style={{margin: 4}}>{this.state.options.reminder.value}</Chip>
-    }
-*/
-  }
-
-  showLineOptions() {
-    if (!this.state.last) {
-      return (
-        <div style={{width: '100%', margin: 0, height: 30, display: 'inline-flex'}}>
-          <IconButton
-            tooltip="Remind me on"
-            tooltipPosition="top-right"
-            className="line-buttons"
-            style={iconStyles.iconArea}
-            iconStyle={iconStyles.icon} 
-            onClick={this.handleClick.bind(this)} >
-            <ActionTouchApp />
-          </IconButton>
-          {/* TODO: Add onChange functions and values for set as important and highlight color*/}
-          <IconMenu
-            onChange={this.handleChangeMenu.bind(this, 'important')}
-            value={this.state.options.important.value}
-            iconButtonElement={(
-              <IconButton
-                tooltip="Set as important"
-                tooltipPosition="top-right"
-                className="line-buttons"
-                style={iconStyles.iconArea}
-                iconStyle={iconStyles.icon}>
-                  <ToggleStar />
-              </IconButton>
-            )}
-            anchorOrigin={{horizontal: 'left', vertical: 'top'}}
-            targetOrigin={{horizontal: 'left', vertical: 'top'}}>
-              <MenuItem value="0" primaryText="Normal priority" />
-              <MenuItem value="1" leftIcon={<ToggleStar color={amber700}/>} primaryText="Priority 1" />
-              <MenuItem value="2" leftIcon={<ToggleStar color={amber400}/>} primaryText="Priority 2" />
-              <MenuItem value="3" leftIcon={<ToggleStar color={amber100}/>} primaryText="Priority 3" />
-          </IconMenu>
-          <IconMenu
-            onChange={this.handleChangeMenu.bind(this, 'highlight')}
-            value={this.state.options.highlight.value}
-            iconButtonElement={(
-              <IconButton
-                tooltip="Highlight"
-                tooltipPosition="top-right"
-                className="line-buttons"
-                style={iconStyles.iconArea}
-                iconStyle={iconStyles.icon} >
-                <EditorHighlight />
-              </IconButton>
-            )}            
-            anchorOrigin={{horizontal: 'left', vertical: 'top'}}
-            targetOrigin={{horizontal: 'left', vertical: 'top'}}>
-              <MenuItem value="0" leftIcon={<ImageLens color="transparent"/>} primaryText="No color" />
-              <MenuItem value="1" leftIcon={<ImageLens color={yellowA100}/>} primaryText="Yellow" />
-              <MenuItem value="2" leftIcon={<ImageLens color={blueA100}/>} primaryText="Blue" />
-              <MenuItem value="3" leftIcon={<ImageLens color={greenA100}/>} primaryText="Green" />
-              <MenuItem value="4" leftIcon={<ImageLens color={orangeA100}/>} primaryText="Orange" />
-              <MenuItem value="5" leftIcon={<ImageLens color={cyanA100}/>} primaryText="Blue-green" />
-          </IconMenu>
-          <IconButton
-            tooltip="Move to history"
-            tooltipPosition="top-right"
-            className="line-buttons"
-            style={iconStyles.iconArea}
-            iconStyle={iconStyles.icon} >
-            <ContentMoveToInbox />
-          </IconButton>
-          <IconButton
-            tooltip="More options"
-            tooltipPosition="top-right"
-            className="line-buttons"
-            style={iconStyles.iconArea}
-            iconStyle={iconStyles.icon} >
-            <NavigationMoreVert /> 
-          </IconButton> 
-          {this.showChips()}
-        </div>
-      );
-    }
   }
 
   handleChange(e) {
@@ -272,94 +154,7 @@ export default class NoteLine extends React.Component {
     this.props.deleteLine();
   }
 
-  handleClick(e) {
-    
-  }
-
-  handleChangeMenu(type, e, value) {
-    switch(type) {
-    case 'important':
-      if (+value === 0) {
-        return this.setState({
-          options: {
-          important: {
-            set: false, 
-            value: value, 
-            color: importantColors[value]
-          },
-          reminder: {
-            ...this.state.options.reminder
-          },
-          highlight: {
-            ...this.state.options.highlight
-          },
-        }})
-
-      }
-            
-      this.setState({
-        options:{
-          important: {
-            set: true,
-            value: value,
-            color: importantColors[value]
-          },
-          reminder: {
-            ...this.state.options.reminder
-          },
-          highlight: {
-            ...this.state.options.highlight
-          },
-          
-          
-        }
-        
-      }) 
-      console.log(this.state.options);
-      break;
-    case 'highlight':
-      if (+value === 0) {
-        console.log('highlight', this.state.options.highlight, value, highlightColors[value+1])
-        return this.setState({
-          options: {
-            highlight: {
-              set: false, 
-              value: value, 
-              color: highlightColors[value]
-            },
-            reminder: {
-              ...this.state.options.reminder
-            },
-            important: {
-              ...this.state.options.important
-            }
-          }
-        })
-
-      }
-      console.log('highlight', this.state.options.highlight, value, highlightColors[value+1])
-      this.setState({
-          options: {
-            highlight: {
-              set: true,
-              value: value,
-              color: highlightColors[+value]
-            },
-            reminder: {
-              ...this.state.options.reminder
-            },
-            important: {
-              ...this.state.options.important
-            }
-          }
-      }) 
-      console.log('highlight', this.state.options.highlight, value)
-      break;
-    }
-
-  }
-
-  renderLine() {
+  getLine() {
     return (
     <TextField 
       onChange={this.handleChange.bind(this)}
@@ -377,7 +172,7 @@ export default class NoteLine extends React.Component {
     );
   }
 
-  showLine() {
+  renderLine() {
     if (this.isImportant()) {
       return (
         <Badge 
@@ -385,28 +180,61 @@ export default class NoteLine extends React.Component {
             style={{width: '94%', margin: 0, padding: 0}}
             badgeStyle={{backgroundColor: this.state.options.important.color, left: 343, margin: 0, padding: 0}}
             >
-            {this.renderLine()}
+            {this.getLine()}
         </Badge>
         )
     } 
 
-    return this.renderLine();
+    return this.getLine();
+  }
 
+  renderLineOptions() {
+    if (!this.state.last) {
+      return <NoteLineOptions
+                reminder={this.state.options.reminder}
+                highlight={this.state.options.highlight}
+                important={this.state.options.important}
+                updateOptions={this.updateOptions.bind(this)}
+                />
+    }
+  }
+
+  renderCancelButton() {
+    if (!this.state.last) {
+      return (
+        <IconButton
+          tooltip="Delete line"
+          className="line-buttons"
+          style={inlineIconStyle.iconArea} 
+          iconStyle={inlineIconStyle.icon} 
+          onClick={this.handleClickDelete.bind(this)} >
+          <NavigationCancel />
+        </IconButton>
+      );
+    }
+  }
+
+  renderAddIcon() {
+    if (this.state.last) {
+      return (
+        <ContentAdd style={{left: 5, height: 18, width: 18, color: 'grey', paddingTop: 13, paddingRight: 2, marginRight: 5, marginLeft: 5}}/>
+      );
+    }
   }
 
   render() {
     return (
       <div className="note-line-container" style={this.state.last ? lineOutHover.last : lineOutHover.notLast}>
         <div className="line-w-button" tabIndex="0">
-          {this.showAddIcon()}
+          {this.renderAddIcon()}
 
-          {this.showLine()}
+          {this.renderLine()}
 
-          {this.showCancelButton()}
+          {this.renderCancelButton()}
           
         </div>
 
-        {this.showLineOptions()}
+        {this.renderLineOptions()}
         
       </div>
     );
---
+++ NoteLineOptions.js
@@ -0,0 +1,140 @@
+import React from 'react';
+
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import Chip from 'material-ui/Chip';
+
+import ActionTouchApp from 'material-ui/svg-icons/action/touch-app';
+import ToggleStar from 'material-ui/svg-icons/toggle/star';
+import EditorHighlight from 'material-ui/svg-icons/editor/highlight';
+import ContentMoveToInbox from 'material-ui/svg-icons/content/move-to-inbox';
+import NavigationMoreVert from 'material-ui/svg-icons/navigation/more-vert';
+import ImageLens from 'material-ui/svg-icons/image/lens';
+
+import {
+  amber700,
+  amber400,
+  amber100,
+  yellowA100,
+  blueA100,
+  greenA100,
+  orangeA100,
+  cyanA100
+} from 'material-ui/styles/colors'
+
+import { iconStyles, importantColors, highlightColors } from './NoteLine'
+
+export default class NoteLineOptions extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      reminder: props.reminder,
+      highlight: props.highlight,
+      important: props.important
+    }
+  }
+
+  handleChange(type, e, value) {
+    switch (type) {
+    case 'highlight':
+      let newStateHighlight = {set: value !== 0, color: highlightColors[+value], value: value}
+      this.setState({highlight: newStateHighlight});
+      this.props.updateOptions({highlight: newStateHighlight});
+      break;
+    case 'important':
+      let newStateImportant = {set: value !== 0, color: importantColors[+value], value: value}
+      this.setState({important: newStateImportant})
+      this.props.updateOptions({important: newStateImportant})
+      break;
+    }
+  }
+
+
+  showChips() {
+    if (this.props.reminder.set) {
+      return (
+        <Chip 
+          style={{height: 20, marginTop: 7}}
+          labelStyle={{padding: 4, top: 14, margin: 4, marginTop: 0, paddingTop: 0, lineHeight: '1.6em', height: 20}}>
+      </Chip>)
+    }
+  }
+
+  render() {
+    return (
+        <div style={{width: '100%', margin: 0, height: 30, display: 'inline-flex'}}>
+          <IconButton
+            tooltip="Remind me on"
+            tooltipPosition="top-right"
+            className="line-buttons"
+            style={iconStyles.iconArea}
+            iconStyle={iconStyles.icon} >
+            <ActionTouchApp />
+          </IconButton>
+          {/* TODO: Add onChange functions and values for set as important and highlight color*/}
+          <IconMenu
+            onChange={this.handleChange.bind(this, 'important')}
+            value={this.state.important.value}
+            iconButtonElement={(
+              <IconButton
+                tooltip="Set as important"
+                tooltipPosition="top-right"
+                className="line-buttons"
+                style={iconStyles.iconArea}
+                iconStyle={iconStyles.icon}>
+                  <ToggleStar />
+              </IconButton>
+            )}
+            anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+            targetOrigin={{horizontal: 'left', vertical: 'top'}}>
+              <MenuItem value="0" primaryText="Normal priority" />
+              <MenuItem value="1" leftIcon={<ToggleStar color={amber700}/>} primaryText="Priority 1" />
+              <MenuItem value="2" leftIcon={<ToggleStar color={amber400}/>} primaryText="Priority 2" />
+              <MenuItem value="3" leftIcon={<ToggleStar color={amber100}/>} primaryText="Priority 3" />
+          </IconMenu>
+          <IconMenu
+            onChange={this.handleChange.bind(this, 'highlight')}
+            value={this.state.highlight.value}
+            iconButtonElement={(
+              <IconButton
+                tooltip="Highlight"
+                tooltipPosition="top-right"
+                className="line-buttons"
+                style={iconStyles.iconArea}
+                iconStyle={iconStyles.icon} >
+                <EditorHighlight />
+              </IconButton>
+            )}            
+            anchorOrigin={{horizontal: 'left', vertical: 'top'}}
+            targetOrigin={{horizontal: 'left', vertical: 'top'}}>
+              <MenuItem value="0" leftIcon={<ImageLens color="transparent"/>} primaryText="No color" />
+              <MenuItem value="1" leftIcon={<ImageLens color={yellowA100}/>} primaryText="Yellow" />
+              <MenuItem value="2" leftIcon={<ImageLens color={blueA100}/>} primaryText="Blue" />
+              <MenuItem value="3" leftIcon={<ImageLens color={greenA100}/>} primaryText="Green" />
+              <MenuItem value="4" leftIcon={<ImageLens color={orangeA100}/>} primaryText="Orange" />
+              <MenuItem value="5" leftIcon={<ImageLens color={cyanA100}/>} primaryText="Blue-green" />
+          </IconMenu>
+          <IconButton
+            tooltip="Move to history"
+            tooltipPosition="top-right"
+            className="line-buttons"
+            style={iconStyles.iconArea}
+            iconStyle={iconStyles.icon} >
+            <ContentMoveToInbox />
+          </IconButton>
+          <IconButton
+            tooltip="More options"
+            tooltipPosition="top-right"
+            className="line-buttons"
+            style={iconStyles.iconArea}
+            iconStyle={iconStyles.icon} >
+            <NavigationMoreVert /> 
+          </IconButton> 
+          {/*this.showChips()*/}
+        </div>
+      );
+  } 
+}
+
