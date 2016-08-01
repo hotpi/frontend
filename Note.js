51fdaa1616e31a03720446b80bbed33e9e715abb
juan <juanecabellob@gmail.com>
2016-08-01 21:07:23 +0200

    Set some view conditions. Timestamp paper should only be seen if it's a history or a todo note.

--- Note.js
+++ Note.js
@@ -190,7 +190,7 @@ export default class Note extends React.Component {
           </Paper>
           <Paper  
             zDepth={1}
-            style={{width: 120, height: 40, marginLeft: 1, display: this.props.type === "History" ? 'inline-flex': 'none'}}>
+            style={{width: 120, height: 40, marginLeft: 1, display: this.props.type === "History" || this.props.type === "Todo" ? 'inline-flex': 'none'}}>
             <DeviceAccessTime style={{color: 'grey', height: 24, width: 24, marginLeft: 10, marginTop: 10}}/><h3 style={{padding: 12, margin: 0, textAlign: 'center', fontWeight: 100}}>Today</h3>
           </Paper>
         </div>
