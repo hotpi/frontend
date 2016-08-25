9c88c1ec6a2b26cfa74530a1a461a3c913719a17
juan <juanecabellob@gmail.com>
2016-08-25 19:17:30 +0200

    Added API calls files, not integrated yet.

---
+++ api.js
@@ -0,0 +1,121 @@
+import { Schema, arrayOf, normalize } from 'normalizr'
+import 'isomorphic-fetch'
+
+const API_ROOT = 'https://api.github.com/'
+
+// Fetches an API response and normalizes the result JSON according to schema.
+// This makes every API response have the same shape, regardless of how nested it was.
+function callApi(endpoint, schema) {
+  const fullUrl = (endpoint.indexOf(API_ROOT) === -1) ? API_ROOT + endpoint : endpoint
+
+  return fetch(fullUrl)
+    .then(response =>
+      response.json().then(json => ({ json, response }))
+    ).then(({ json, response }) => {
+      if (!response.ok) {
+        return Promise.reject(json)
+      }
+
+      const camelizedJson = camelizeKeys(json)
+
+      return Object.assign({},
+        normalize(json, schema)
+      )
+    })
+}
+
+// We use this Normalizr schemas to transform API responses from a nested form
+// to a flat form where repos and users are placed in `entities`, and nested
+// JSON objects are replaced with their IDs. This is very convenient for
+// consumption by reducers, because we can easily build a normalized tree
+// and keep it updated as we fetch more data.
+
+// Read more about Normalizr: https://github.com/paularmstrong/normalizr
+
+// GitHub's API may return results with uppercase letters while the query
+// doesn't contain any. For example, "someuser" could result in "SomeUser"
+// leading to a frozen UI as it wouldn't find "someuser" in the entities.
+// That's why we're forcing lower cases down there.
+
+const patientsSchema = new Schema('patients', {
+  idAttribute: patient => patient.ID
+})
+
+patientsSchema.define({
+  notes: arrayOf(noteSchema)
+})
+
+const noteSchema = new Schema('notes', {
+  idAttribute: note => note.ID
+})
+
+noteSchema.define({
+  noteLines: arrayOf(noteLineSchema)
+})
+
+const noteLineSchema = new Schema('noteLines', {
+  idAttribute: noteLine => noteLine.ID
+})
+
+// Schemas for Github API responses.
+export const Schemas = {
+  PATIENT: patientsSchema,
+  PATIENT_ARRAY: arrayOf(patientsSchema),
+  NOTE: noteSchema,
+  NOTE_ARRAY: arrayOf(noteSchema),
+  NOTELINE: noteSchema,
+  NOTELINE_ARRAY: arrayOf(noteSchema),
+
+}
+
+// Action key that carries API call info interpreted by this Redux middleware.
+export const CALL_API = Symbol('Call API')
+
+// A Redux middleware that interprets actions with CALL_API info specified.
+// Performs the call and promises when such actions are dispatched.
+export default store => next => action => {
+  const callAPI = action[CALL_API]
+  if (typeof callAPI === 'undefined') {
+    return next(action)
+  }
+
+  let { endpoint } = callAPI
+  const { schema, types } = callAPI
+
+  if (typeof endpoint === 'function') {
+    endpoint = endpoint(store.getState())
+  }
+
+  if (typeof endpoint !== 'string') {
+    throw new Error('Specify a string endpoint URL.')
+  }
+  if (!schema) {
+    throw new Error('Specify one of the exported Schemas.')
+  }
+  if (!Array.isArray(types) || types.length !== 3) {
+    throw new Error('Expected an array of three action types.')
+  }
+  if (!types.every(type => typeof type === 'string')) {
+    throw new Error('Expected action types to be strings.')
+  }
+
+  function actionWith(data) {
+    const finalAction = Object.assign({}, action, data)
+    delete finalAction[CALL_API]
+    return finalAction
+  }
+
+  const [ requestType, successType, failureType ] = types
+  next(actionWith({ type: requestType }))
+
+  return callApi(endpoint, schema).then(
+    response => next(actionWith({
+      response,
+      type: successType
+    })),
+    error => next(actionWith({
+      type: failureType,
+      error: error.message || 'Something bad happened'
+    }))
+  )
+}
